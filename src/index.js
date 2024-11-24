cb5e98b1db08502422ed0509dd5db71f8a1a57c4
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-11-24 11:54:57 -0500

    refactor: use left position instead of scrolling to view

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ export { PosterSlider };
  * @param {*} options  an object which contains { width: <optional-string>, height: <optional-string>, parentSelector: <optional-css-selector-string}.
  */
 class PosterSlider {
+  static dataID = "data-slider-id";
+
   constructor(docObj, imgElementsArr, options) {
     this.docObj = docObj;
     this.imgElementsArr = imgElementsArr;
     const defaults = {
-      width: "426px",
-      height: "640px",
+      width: 426, //in pixels
+      height: 640, //in pixels
       parentSelector: "body",
       slideTimer: 5000,
       "aspect-ratio": "2 / 3",
@@ -22,7 +24,7 @@ class PosterSlider {
 
     this.currentSlide = 0;
     let root = this.docObj.documentElement;
-    root.style.setProperty("--poster-width", this.options.width);
+    root.style.setProperty("--poster-width", `${this.options.width}px`);
     root.style.setProperty("--aspect-ratio", this.options["aspect-ratio"]);
 
     this.init();
@@ -30,41 +32,60 @@ class PosterSlider {
   }
 
   startSlideShow = () => {
-    this.slide(1);
+    this.slide(-1);
+  };
+
+  navigate(e) {
+    if (e.target.hasAttribute(PosterSlider.dataID)) {
+      const id = e.target.getAttribute(PosterSlider.dataID);
+      this.currentSlide = Number(id.slice(5));
+      this.moveTo(this.currentSlide);
+
+      this.syncNavigation();
+    }
+  }
+
+  moveTo = (slideNumber) => {
+    if (slideNumber === 0) {
+      this.setLeftPos(this.posterSlider, 0);
+    } else {
+      this.setLeftPos(
+        this.posterSlider,
+        slideNumber * -1 * Number(this.options.width)
+      );
+    }
   };
 
   slide = (value) => {
-    let useScroll = true;
-    let targetSlide = this.currentSlide + value;
+    let useScroll = true; //it is true if we are scrolling back and forth and false if we reached the first or last image
+    let targetSlide = this.currentSlide - value;
     if (targetSlide < 0) {
-      targetSlide = this.imgElementsArr.length - 1;
+      targetSlide = this.imgElementsArr.length - 1; //if at the start, jump to the end immediately
       useScroll = false;
     } else if (targetSlide === this.imgElementsArr.length) {
-      targetSlide = 0;
+      targetSlide = 0; //if at the end, just to the start immediately
       useScroll = false;
     }
 
-    const posterElList = this.docObj.querySelectorAll(
-      `${this.options.parentSelector} .poster`
-    );
-    Object.values(posterElList).forEach((el) => {
-      if (Number(el.getAttribute("id").slice(5)) === targetSlide) {
-        if (useScroll) {
-          el.scrollIntoView({ behavior: "smooth", inline: "start" });
-        } else {
-          window.location.href = `#${el.getAttribute("id")}`;
-        }
-      }
-    });
-
+    if (useScroll) {
+      const currentpos = this.posterSlider.style.left.slice(0, -2);
+      const newVal = +currentpos + value * Number(this.options.width);
+      this.setLeftPos(this.posterSlider, newVal);
+    } else {
+      this.moveTo(targetSlide);
+    }
     this.currentSlide = targetSlide;
     this.syncNavigation();
   };
 
+  setLeftPos(el, num) {
+    el.style.left = `${num}px`;
+  }
+
   syncNavigation() {
     Object.values(this.navigationEl.children).forEach((button) => {
       if (
-        Number(button.getAttribute("data-slider-id").slice(5)) ===
+        Number(button.getAttribute(PosterSlider.dataID).slice(5)) ===
         this.currentSlide
       ) {
         button.style.backgroundColor = getComputedStyle(
@@ -92,14 +113,15 @@ class PosterSlider {
     posterFrameEl.appendChild(backBtn);
 
     backBtn.addEventListener("click", () => {
-      this.slide(-1);
+      this.slide(1);
     });
 
     const sliderDiv = this.docObj.createElement("div");
     sliderDiv.classList.add("poster-slider");
+    this.posterSlider = sliderDiv;
 
     for (let i = 0; i < this.imgElementsArr.length; i++) {
-      this.imgElementsArr[i].setAttribute("data-slider-id", `post-${i}`);
+      this.imgElementsArr[i].setAttribute(PosterSlider.dataID, `post-${i}`);
       const spanEl = this.docObj.createElement("span");
       spanEl.classList.add("poster");
       spanEl.setAttribute("id", `post-${i}`);
@@ -116,7 +138,7 @@ class PosterSlider {
       import.meta.url
     )}" alt="slide forward">`;
     forthBtn.classList.add("forth");
-    forthBtn.addEventListener("click", () => this.slide(1));
+    forthBtn.addEventListener("click", () => this.slide(-1));
     posterFrameEl.appendChild(forthBtn);
 
     this.navigationEl = this.docObj.createElement("div");
@@ -128,7 +150,7 @@ class PosterSlider {
       const buttonEl = this.docObj.createElement("button");
       buttonEl.setAttribute("type", "button");
       buttonEl.setAttribute("name", "nav-button");
-      buttonEl.setAttribute("data-slider-id", `post-${i}`);
+      buttonEl.setAttribute(PosterSlider.dataID, `post-${i}`);
       if (i === 0) {
         buttonEl.style.backgroundColor = getComputedStyle(
           parentEl
@@ -139,14 +161,5 @@ class PosterSlider {
     posterFrameEl.appendChild(this.navigationEl);
 
     parentEl.appendChild(posterFrameEl);
-  }
-
-  navigate(e) {
-    const id = e.target.getAttribute("data-slider-id");
-    const el = this.docObj.querySelector(`#${id}`);
-    el.scrollIntoView({ behavior: "smooth", inline: "start" });
-
-    this.currentSlide = Number(id.slice(5));
-    this.syncNavigation();
   }
 }
